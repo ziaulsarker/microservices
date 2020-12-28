552a1090ad09a5e7de77d2e9feedfa60ff90e94f
ziaulsarker <ziaulsarker@gmail.com>
2020-12-28 00:51:08 -0500

    update component

---
+++ client/src/components/postCreate.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useRef, useContext } from 'react';
+import {PostsContext} from "../contexts/posts-context";
+import {UseFetchPostsInterfaces} from "../types/post-type";
+
+const PostCreate = () : React.ReactElement => {
+    const [formData, setFormData]: any = useState({});
+    const [postTitle, setPostTitle]: any = useState("");
+
+    const formRef = useRef(document.createElement('form'));
+    const {setPosts} : UseFetchPostsInterfaces | any = useContext(PostsContext)
+
+
+    const handleSubmitFormSubmit = async (event: any) => {
+        event.preventDefault();
+        const postEndpoint = 'http://127.0.0.1:3001/posts'
+        const postOptions = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        }
+        const response = await fetch(postEndpoint, postOptions);
+        const posts = await response.json();
+
+        setPosts(posts);
+    }
+    
+    const handleTitleInputChange = async (event: any) => {
+        const { value } = event.target;
+        const formData = {title: value};
+        console.log("val => ", value);
+        console.log("formData => ", formData);
+
+        setPostTitle(value);
+        setFormData(formData);
+    }
+    
+
+    return (
+        <>
+        <h2> Create Post </h2>
+        <form ref={formRef} onSubmit={handleSubmitFormSubmit}>
+            <input onChange={handleTitleInputChange} type="text" placeholder="Create a Post" value={postTitle} />
+            <input type="submit" value="Create"/>
+        </form>
+        </>
+    )
+}
+
+export default PostCreate
